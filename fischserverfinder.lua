a570513278b79207752409bdec0d8c01f23ec8f8
penguin586970 <136866320+P3nguinMinecraft@users.noreply.github.com>
2025-01-22 22:48:46 -0500

    doubled the tp search to make it more reliable

--- fischserverfinder.lua
+++ fischserverfinder.lua
@@ -490,6 +490,17 @@ function sunkenchesttp1()
         if not foundchest then
             scanchest()
         end
+        for _, coords in ipairs(sunkenchestcoords) do
+            tp(coords.x, coords.y + 70, coords.z)
+            task.wait(0.01)
+            if not checkteleporting then
+                foundchest = true
+                break
+            end
+        end
+        if not foundchest then
+            scanchest()
+        end
         checkteleporting = false
     end)
 
