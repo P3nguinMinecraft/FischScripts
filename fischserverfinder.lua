850ddd11a5e13476779ff1838272af504f5eb6f4
penguin586970 <136866320+P3nguinMinecraft@users.noreply.github.com>
2025-01-22 21:32:07 -0500

    speed up more!

--- fischserverfinder.lua
+++ fischserverfinder.lua
@@ -481,7 +481,7 @@ function sunkenchesttp1()
         checkteleporting = true
         for _, coords in ipairs(sunkenchestcoords) do
             tp(coords.x, coords.y + 70, coords.z)
-            task.wait(0.1)
+            task.wait(0.01)
             if not checkteleporting then
                 foundchest = true
                 break
