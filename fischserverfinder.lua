da4a886c91f1c2bc7687c0af345da2aba8e288a9
penguin586970 <136866320+P3nguinMinecraft@users.noreply.github.com>
2025-02-01 11:14:50 -0500

    Update fischserverfinder.lua

--- fischserverfinder.lua
+++ fischserverfinder.lua
@@ -1,2 +1,2 @@
 setclipboard("discord.gg/fWncS2vFxn")
-game.Players.LocalPlayer:Kick("[FSF] FischServerFinder has migrated. Join the discord server! (Link copied to clipboard)")
+game.Players.LocalPlayer:Kick("FischServerFinder has migrated. Join the discord server! (Link copied to clipboard) -Penguin")
